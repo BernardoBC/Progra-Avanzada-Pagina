49658aaf1065fdb40ed303e05771c62799947a25
BernardoBC <bernardobonillac@gmail.com>
2014-08-04 17:32:34 -0600

    Citas - quitar fillTable()

--- Citas.aspx.cs
+++ Citas.aspx.cs
@@ -13,7 +13,6 @@ public partial class Citas : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        fillTable();
     }
     protected void Button1_Click(object sender, EventArgs e)
     {
@@ -36,27 +35,5 @@ public partial class Citas : System.Web.UI.Page
         
         Response.Redirect(Request.RawUrl);
          
-    }
-    private void fillTable() {        
-        /*SqlConnection sqlconnect = new SqlConnection("Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\\Database.mdf;Integrated Security=True");
-        string stmt = "select * from Citas";
-        SqlCommand sqlCommand = new SqlCommand(stmt, sqlconnect);
-        sqlconnect.Open();
-
-        SqlDataReader reader = sqlCommand.ExecuteReader();
-        if (reader.HasRows)
-        {
-            while (reader.Read())
-            {
-                Console.WriteLine("{0}\t{1}", reader.GetInt32(0),
-                    reader.GetString(1));
-            }
-        }
-        else
-        {
-            Console.WriteLine("No rows found.");
-        }
-
-        sqlconnect.Close(); */      
     }
 }
