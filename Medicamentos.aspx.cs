cbe200703e4a33c02dfb6c5dd7cfc4d024f09eb0
pipechava <pipechava@gmail.com>
2014-08-04 20:27:53 -0600

    no message

--- Medicamentos.aspx.cs
+++ Medicamentos.aspx.cs
@@ -4,11 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using System.IO;
+using System.Configuration;
 
 public partial class Medicamentos : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+
+        String Nombre = entradaNombre.Value;
+        String cantidad = entraCantidad.Value;
+        String Precio = Precio.Value;
+        String Descripcion = entradaDescripcion.Value;
 
     }
 }
